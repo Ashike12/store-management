a56fbbec69c1e131fd07c1d2fc9006b964c245bb
Ashikur Rahman Nabir <nabir.rahman@selisegroup.com>
2025-03-20 22:25:29 +0600

    Updates

---
+++ assets/dashboard-B7fTarlf.js
@@ -0,0 +1,1 @@
+import{j as a,T as e}from"./index-bB_UOkb9.js";function s(){return a.jsx("div",{className:"flex w-full flex-col gap-5",children:a.jsx(e,{content:"Dashboard"})})}export{s as default};
